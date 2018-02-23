eed2493be790b948d47816ba32e8e1db6d8f7fc4
Robin Da Silva <robindasilva@Robins-MacBook-Pro.local>
2018-02-23 16:49:28 +0000

    slick

--- src/js/Todolist.js
+++ src/js/Todolist.js
@@ -10,9 +10,43 @@ function displayText() {
 
 }
 
+
 function removeItem() {
   var candidate = document.getElementById("myText");
   var item = document.getElementById(myText.value);
   ul.removeChild(item);
+}
+
+
+function addText(){
+    var input = document.getElementById('inputTask').value;
+    var node=document.createElement("p");
+    var textnode=document.createTextNode(input);
+    node.appendChild(textnode);
+    document.getElementById('addTask').appendChild(node);
+
+    var removeTask = document.createElement('input');
+    removeTask.setAttribute('type', 'button');
+    removeTask.setAttribute("value", "Remove");
+    removeTask.setAttribute("id", "removeButton");
+    removeTask.addEventListener('click', function(e) {
+        node.parentNode.removeChild(node);
+    }, false);
+    node.appendChild(removeTask);
+
 
 }
+
+$(function() {
+  $( "#datepicker" ).datepicker({
+    maxDate: 5
+  } );
+} );
+
+$(function(){
+  $('.carousel').slick({
+    slidesToShow: 4,
+    dots:true,
+    centerMode: true,
+  });
+});
