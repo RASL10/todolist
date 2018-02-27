396fd7daeb1321b4dd3c6f7f818c3b8f349a34bf
Robin Da Silva <robindasilva@Robins-MacBook-Pro.local>
2018-02-27 18:50:15 +0000

    remove

--- src/js/Todolist.js
+++ src/js/Todolist.js
@@ -1,40 +1,18 @@
-var ul = document.getElementById('demo');
-var lastid = 0;
-
 function displayText() {
-  var myText = document.getElementById('myText')
+  var input = document.getElementById('myText').value;
   var li = document.createElement("li");
-  li.setAttribute('id', myText.value);
-  li.appendChild(document.createTextNode(myText.value));
-  ul.appendChild(li);
+  var textnode = document.createTextNode(input)
+  li.appendChild(textnode);
+  document.getElementById('demo').appendChild(li)
 
-}
-
-
-function removeItem() {
-  var candidate = document.getElementById("myText");
-  var item = document.getElementById(myText.value);
-  ul.removeChild(item);
-}
-
-
-function addText(){
-    var input = document.getElementById('inputTask').value;
-    var node=document.createElement("p");
-    var textnode=document.createTextNode(input);
-    node.appendChild(textnode);
-    document.getElementById('addTask').appendChild(node);
-
-    var removeTask = document.createElement('input');
-    removeTask.setAttribute('type', 'button');
-    removeTask.setAttribute("value", "Remove");
-    removeTask.setAttribute("id", "removeButton");
-    removeTask.addEventListener('click', function(e) {
-        node.parentNode.removeChild(node);
-    }, false);
-    node.appendChild(removeTask);
-
-
+  var removeTask = document.createElement('input');
+  removeTask.setAttribute('type', 'button');
+  removeTask.setAttribute("value", "X");
+  removeTask.setAttribute("id", "removeButton");
+  removeTask.addEventListener('click', function(e) {
+      li.parentNode.removeChild(li);
+  });
+  li.appendChild(removeTask);
 }
 
 $(function() {
@@ -42,11 +20,3 @@ $(function() {
     maxDate: 5
   } );
 } );
-
-$(function(){
-  $('.carousel').slick({
-    slidesToShow: 4,
-    dots:true,
-    centerMode: true,
-  });
-});
