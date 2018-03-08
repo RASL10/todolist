b5d2da45b2b4108845d7c4abde659d138425c086
Robin Da Silva <robindasilva@Robins-MacBook-Pro.local>
2018-03-08 00:45:30 +0000

    edit function

--- src/js/Todolist.js
+++ src/js/Todolist.js
@@ -1,23 +1,57 @@
 function displayText() {
   var input = document.getElementById('myText').value;
   var li = document.createElement("li");
+  var textbox = document.createElement('input');
+  var label = document.createElement('label');
   var textnode = document.createTextNode(input);
-  li.classList.add("yolo");
-  li.appendChild(textnode);
-  li.addEventListener('click', function() {
-        li.classList.toggle("checked");
-    }, false);
+  textbox.setAttribute('type', 'text');
+  textbox.setAttribute('value', document.getElementById('myText').value)
+  li.appendChild(textbox);
+  label.appendChild(textnode);
+  li.appendChild(label);
+
+  li.setAttribute('id', 'edit');
+
   document.getElementById('demo').appendChild(li)
 
+  var editTask = document.createElement('input');
+  editTask.setAttribute('type', 'button');
+  editTask.setAttribute("value", "Edit");
+  li.appendChild(editTask);
+  editTask.addEventListener('click', function(){
+    var listItem=editTask.parentNode;
+    var textbox=listItem.querySelector('input[type=text]');
+    var label=listItem.querySelector("label");
+    var containsClass=li.classList.contains("editMode");
+    		//If class of the parent is .editmode
+    		if(containsClass){
 
-  var removeTask = document.createElement('input');
-  removeTask.setAttribute('type', 'button');
-  removeTask.setAttribute("value", "X");
-  removeTask.setAttribute("id", "removeButton");
-  removeTask.addEventListener('click', function(e) {
+    		//switch to .editmode
+    		//label becomes the inputs value.
+label.innerText=textbox.value;
+
+    		}else{
+textbox.value=label.innerText;
+
+    		}
+
+    		//toggle .editmode on the parent.
+    		listItem.classList.toggle("editMode");
+
+
+});
+
+
+var removeTask = document.createElement('input');
+removeTask.setAttribute('type', 'button');
+removeTask.setAttribute("value", "X");
+removeTask.setAttribute("id", "removeButton");
+removeTask.addEventListener('click', function() {
       li.parentNode.removeChild(li);
-  });
-  li.appendChild(removeTask);
+
+});
+li.appendChild(removeTask);
+
 }
 
 $(function() {
@@ -25,3 +59,8 @@ $(function() {
     maxDate: 5
   } );
 } );
+
+
+li.addEventListener('click', function() {
+      li.classList.toggle("checked");
+  }, false);
