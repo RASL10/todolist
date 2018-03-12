389d583b6ad6f91076399b87b1da4450682eb678
Robin Da Silva <robindasilva@Robins-MacBook-Pro.local>
2018-03-12 16:59:10 +0000

    date

--- src/js/Todolist.js
+++ src/js/Todolist.js
@@ -1,21 +1,48 @@
+
+
+function displayDate() {
+  var dateInput = document.getElementById('datepicker').value;
+  var dateLabel = document.createElement('label');
+  var dateTextNode = document.createTextNode(dateInput);
+  var li = document.createElement("li");
+  dateLabel.appendChild(dateTextNode)
+  li.appendChild(dateLabel)
+  document.getElementById('date').appendChild(li)
+
+
+  var removeTask = document.createElement('input');
+  removeTask.setAttribute('type', 'button');
+  removeTask.setAttribute("value", "X");
+  removeTask.setAttribute("id", "removeButton");
+  removeTask.addEventListener('click', function() {
+        li.parentNode.removeChild(li);
+  });
+  li.appendChild(removeTask);
+}
+
+
+
 function displayText() {
   var input = document.getElementById('myText').value;
   var li = document.createElement("li");
   var textbox = document.createElement('input');
   var label = document.createElement('label');
   var textnode = document.createTextNode(input);
+
   textbox.setAttribute('type', 'text');
   textbox.setAttribute('value', document.getElementById('myText').value)
   li.appendChild(textbox);
   label.appendChild(textnode);
   li.appendChild(label);
 
+
+
   li.setAttribute('id', 'edit');
   label.addEventListener('click', function() {
         label.classList.toggle("checked");
     }, false);
 
-  document.getElementById('demo').appendChild(li)
+  document.getElementById('list').appendChild(li)
 
   var editTask = document.createElement('input');
   editTask.setAttribute('type', 'button');
@@ -56,3 +83,7 @@ $(function() {
     maxDate: 5
   } );
 } );
+
+function displaycalendar() {
+  $( "#datepicker" ).datepicker('show');
+};
