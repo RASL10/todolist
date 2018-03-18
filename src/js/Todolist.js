efc7b34ffa3e6ff85cb74fb30e2e9c344bc39341
Robin Da Silva <robindasilva@Robins-MacBook-Pro.local>
2018-03-18 20:01:57 +0000

    datepicker functionality

--- src/js/Todolist.js
+++ src/js/Todolist.js
@@ -5,9 +5,19 @@ function displayDate() {
   var dateLabel = document.createElement('label');
   var dateTextNode = document.createTextNode(dateInput);
   var li = document.createElement("li");
+  var dateTextBox = document.createElement('input');
+
+  dateTextBox.setAttribute('type', 'text');
+  dateTextBox.setAttribute('id', "datepickeredit" )
+  li.appendChild(dateTextBox);
   dateLabel.appendChild(dateTextNode)
   li.appendChild(dateLabel)
+
   document.getElementById('date').appendChild(li)
+
+  dateLabel.addEventListener('click', function() {
+        dateLabel.classList.toggle("checked");
+    }, false);
 
 
   var removeTask = document.createElement('input');
@@ -18,7 +28,34 @@ function displayDate() {
         li.parentNode.removeChild(li);
   });
   li.appendChild(removeTask);
+
+  var editDate = document.createElement('input');
+  editDate.setAttribute('type', 'button');
+  editDate.setAttribute("value", "Edit");
+  li.appendChild(editDate);
+  editDate.addEventListener('click', function(){
+    var listItem=editDate.parentNode;
+    var textbox=listItem.querySelector('input[type=text]');
+
+    var label=listItem.querySelector("label");
+    var containsClass=li.classList.contains("editMode");
+    var containschecked=label.classList.contains("checked");
+          if (containsClass) {
+            label.innerText=textbox.value;
+            editDate.setAttribute("value", "edit");
+        } else {
+
+            textbox.value=label.innerText;
+            editDate.setAttribute("value", "ok");
+        }
+    listItem.classList.toggle("editMode");
+
+});
+
 }
+
+
+
 
 
 
@@ -44,7 +81,7 @@ function displayText() {
 
   document.getElementById('list').appendChild(li)
 
-  var editTask = document.createElement('input');
+var editTask = document.createElement('input');
   editTask.setAttribute('type', 'button');
   editTask.setAttribute("value", "Edit");
   li.appendChild(editTask);
@@ -66,12 +103,14 @@ function displayText() {
 });
 
 
+
+
 var removeTask = document.createElement('input');
 removeTask.setAttribute('type', 'button');
 removeTask.setAttribute("value", "X");
 removeTask.setAttribute("id", "removeButton");
 removeTask.addEventListener('click', function() {
-      li.parentNode.removeChild(li);
+li.parentNode.removeChild(li);
 
 });
 li.appendChild(removeTask);
@@ -80,10 +119,41 @@ li.appendChild(removeTask);
 
 $(function() {
   $( "#datepicker" ).datepicker({
-    maxDate: 5
-  } );
+    maxDate: 20,
+  });
 } );
 
+$(document).on('focus', "#datepicker", function () {
+    $(this).datepicker( "destroy" ).datepicker();
+})
+
+$(document).on('focus', "#datepickeredit", function () {
+    $(this).datepicker( "destroy" ).datepicker();
+})
+
+$(function() {
+  $( "#datepickeredit" ).datepicker({
+    maxDate: 20,
+  });
+} );
+
+
+
+
 function displaycalendar() {
-  $( "#datepicker" ).datepicker('show');
+  $( ".datepicker" ).datepicker('show');
 };
+
+function checkValue () {
+  var userNumber = document.getElementById('number').value;
+  var numberText = document.getElementById('hello')
+  if (userNumber > 5) {
+    numberText.classList.add('hello-style')
+    numberText.classList.remove('bye-style')
+  } else if (userNumber < 5) {
+      numberText.classList.remove('hello-style');
+      numberText.classList.add('bye-style')
+  } else {
+    return
+  }
+}
